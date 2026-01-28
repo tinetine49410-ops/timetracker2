afa692ec2c1f3c3cf131bba70c102c2ba6620aeb
tinetine49410-ops <tinetine49410@gmail.com>
2026-01-28 19:07:09 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,3 @@
-const CACHE_NAME = "timetracker-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./app.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
